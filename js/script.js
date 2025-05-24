aa7b48d314ac0889d1b28210b83fdfa885ecfb7b
EdgarPablo <papalarva1@gmail.com>
2025-05-23 23:44:21 -0600

    Se arreglaron errores

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
 function abrirMenu() {
     const menu = document.getElementById("menu");
     const botonMenu = document.getElementById("botonMenu");
-    botonMenu.classList.add('menu__boton--oculto');
+    const body = document.body;
+
+    botonMenu.style.opacity = "0";
+
     setTimeout(() => {
         if (botonMenu.classList.contains('fa-bars')) {
-            
-                botonMenu.classList.remove('fa-bars');
-                botonMenu.classList.add('fa-solid', 'fa-xmark');
-            
+            botonMenu.classList.remove('fa-bars');
+            botonMenu.classList.add('fa-solid', 'fa-xmark');
         } else {
-            
-                botonMenu.classList.remove('fa-solid', 'fa-xmark');
-                botonMenu.classList.add('fa', 'fa-bars', 'menu__boton');
-            
+            botonMenu.classList.remove('fa-solid', 'fa-xmark');
+            botonMenu.classList.add('fa', 'fa-bars', 'menu__boton');
         }
-        botonMenu.classList.remove('menu__boton--oculto');
+        botonMenu.style.opacity = "1";
     }, 500);
-    document.body.classList.toggle("no-scroll");
+
     menu.classList.toggle("menu__visible");
+
+    if (menu.classList.contains("menu__visible")) {
+        body.style.overflow = "hidden";
+    } else {
+        body.style.overflow = "auto";
+    }
 };
 
 function menuDesplegable(id) {
-    if (window.innerWidth >= 768) return;
     const menuConocenos = document.getElementById("menuConocenos");
     const menuAccesos = document.getElementById("menuAccesos");
 
+    if (window.innerWidth >= 768) {
+        menuAccesos.classList.remove("menu__desplegable--visible");
+    menuConocenos.classList.remove("menu__desplegable--visible");
+        return};
     if (id === 1) {
-        menuAccesos.classList.remove("menuVisible");
-        menuConocenos.classList.toggle("menuVisible");
+        menuAccesos.classList.remove("menu__desplegable--visible");
+        menuConocenos.classList.toggle("menu__desplegable--visible");
+
     } else if (id === 2) {
-        menuConocenos.classList.remove("menuVisible");
-        menuAccesos.classList.toggle("menuVisible");
+        menuConocenos.classList.remove("menu__desplegable--visible");
+        menuAccesos.classList.toggle("menu__desplegable--visible");
+
     }
 }
